3e1bf60e5bf981cbc72aecb4d390c316e1fb43fb
Rosalie Newman <rosalienewman0@gmail.com>
2022-10-11 14:52:27 -0400

    tidied code for readability

--- src/index.js
+++ src/index.js
@@ -5,36 +5,24 @@ const restaurantsList = document.getElementById('restaurants-list');
 const usersList = document.getElementById('users-list');
 const reservationsList = document.getElementById('reservations-list');
 
+//////////////////// API GETS
 // Fetch the /api/restaurants route to get restaurants data from database.
 const getRestaurants = async () => {
 	const response = await fetch('/api/restaurants');
 	return await response.json(); // Return it as a json (which needs an await)
 };
-
 // Fetch the /api/users route to get users data from database.
 const getUsers = async () => {
 	const response = await fetch('/api/users');
 	return await response.json(); // Return it as a json
 };
-
-// TODO!
-// /api/users/:userId/reservations
-// Given a userId, gets all of their reservations.
+// /api/users/:userId/reservations  Given a userId, gets all of their reservations.
 const getReservations = async (userId) => {
 	const response = await fetch(`/api/users/${userId}/reservations`);
 	return await response.json();
 };
 
-const renderReservations = (reservations) => {
-	const html = reservations
-		.map(
-			(reservation) =>
-				`<li> Has reservation at ${reservation.restaurant.name}</li>`
-		)
-		.join('');
-	reservationsList.innerHTML = html;
-};
-
+//////////////////// RENDER FUNCTIONS
 // Renders the <li> elements for restaurants, given the <ul> restaurantssList.
 // Using let...of loop and .createElement, innerText, and .appendChild
 // This is just one way to approach how to render the html.
@@ -58,12 +46,36 @@ const renderUsers = (users) => {
 	usersList.innerHTML = html;
 };
 
+const renderReservations = (reservations) => {
+	const html = reservations
+		.map(
+			(reservation) =>
+				`<li> Has reservation at ${reservation.restaurant.name}</li>`
+		)
+		.join('');
+	reservationsList.innerHTML = html;
+};
+
+//////////////////// EVENT LISTENERS
+
+// Listens for when a user's name is clicked.
+// Invokes renderReservations based on the # in the URL.
+window.addEventListener('hashchange', async () => {
+	// Get userId from the URL:
+	// The location.hash property returns the anchor part of a URL,
+	// including the hash sign (#).
+	const userId = window.location.hash.slice(1); // Slice to get rid of # character.
+
+	// Get & render
+	const reservations = await getReservations(userId);
+	renderReservations(reservations);
+});
+//////////////////// INIT
 // A function that invokes all of our functions, like getX() and renderX().
 const init = async () => {
 	try {
 		const rests = await getRestaurants();
 		const users = await getUsers();
-
 		renderUsers(users);
 		renderRestaurants(rests);
 
@@ -81,20 +93,3 @@ const init = async () => {
 };
 
 init();
-
-// Listens for when a user's name is clicked.
-// Invokes renderReservations based on the # in the URL.
-window.addEventListener('hashchange', async () => {
-	// Get userId from the URL:
-	// The location.hash property sets or returns the anchor part of a URL,
-	// including the hash sign (#).
-	const userId = window.location.hash.slice(1); // Slice to get rid of # character.
-
-	// Get the Reservations for userId
-	const reservations = await getReservations(userId);
-
-	// Render
-	renderReservations(reservations);
-});
-
-console.log('hello!');
