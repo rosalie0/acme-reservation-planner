5c8b40f114fb3b350238a23016fdd252e487f516
Rosalie Newman <rosalienewman0@gmail.com>
2022-10-11 13:52:30 -0400

    Put init in a try/catch. renderUsers and renderRests both work, solved with two different ways. getReservations is ToDo.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,57 @@
 // Your code here:
 
-// Use the fetch API to call the appropriate routes to display the data (/api/users and /api/restaurants)
-// Display users in the users-list unordered list and restaurants can be displayed in the restaurants-list unordered list.
-// Create HTML with a template literal by mapping over the users and restaurants.
-const restaurantElement = document.getElementById('restaurants-list');
+// Grab the unordered list (<ul>) elements that we will need for rendering.
+const restaurantsList = document.getElementById('restaurants-list');
+const usersList = document.getElementById('users-list');
+const reservationsList = document.getElementById('reservations-list');
 
+// Fetch the /api/restaurants route to get restaurants data from database.
 const getRestaurants = async () => {
-	const response = await fetch('http://localhost:3000/api/restaurants');
-	const json = await response.json();
-	return json;
+	const response = await fetch('/api/restaurants');
+	return await response.json(); // Return it as a json (which needs an await)
 };
 
+// Fetch the /api/users route to get users data from database.
+const getUsers = async () => {
+	const response = await fetch('/api/users');
+	return await response.json(); // Return it as a json
+};
+
+// TODO!
+// /api/users/:userId/reservations
+const getReservations = async () => {
+	const response = await fetch('/api/');
+};
+
+// Renders the <li> elements for restaurants, given the <ul> restaurantssList.
+// Using let...of loop and .createElement, innerText, and .appendChild
+// This is just one way to approach how to render the html.
 const renderRestaurants = (restaurants) => {
-	for (let rest of restaurants) {
-		const element = document.createElement('li');
-		element.innerText = 'Name: ' + rest.name;
-		console.log(element.innerText);
-		restaurantElement.appendChild(element);
+	for (let restaurant of restaurants) {
+		const element = document.createElement('li'); // Create a new list element
+		element.innerText = restaurant.name; // Give it the restaurant's name
+		restaurantsList.appendChild(element); // Attach current li to the ul.
 	}
 };
 
+// Renders the <li> elements for Users, given the <ul> usersList.
+// Map approach: creates strings for each li, which is given to the <ul>'s innerHTML.
+const renderUsers = (users) => {
+	const html = users.map((user) => `<li> ${user.name} </li>`).join('');
+	usersList.innerHTML = html;
+};
+
+// A function that invokes all of our functions, like getX() and renderX()
 const init = async () => {
-	const rests = await getRestaurants();
-	renderRestaurants(rests);
+	try {
+		const rests = await getRestaurants();
+		const users = await getUsers();
+
+		renderUsers(users);
+		renderRestaurants(rests);
+	} catch (err) {
+		next(err);
+	}
 };
 
 init();
